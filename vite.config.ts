9a3cbb58d57b7e564db535069658067f498da1ae
unknown <costajoshua020@gmail.com>
2026-06-24 23:56:17 +0800

    fix: install core tailwindcss module dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // ◄ 1. Import the Tailwind plugin
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // ◄ 2. Add it right here inside the plugins array
+    tailwindcss(),
   ],
 })
