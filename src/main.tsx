a6bf3c9890635013cae612adfc74f263bef10945
unknown <costajoshua020@gmail.com>
2026-06-24 22:36:14 +0800

    fix: restore standard compiler entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router'
 import App from './app/App'
 import './index.css'
 import '../default_shadcn_theme.css'
 
-// Standard web route setup for single page apps in React Router v7
-const router = createBrowserRouter([
-  {
-    path: '*',
-    element: <App />,
-  }
-], {
-  future: {
-    v7_relativeSplatPath: true,
-  }
-})
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
