6229b8ffc1dedb4a2ce447bc40da12a8a9e92a63
unknown <costajoshua020@gmail.com>
2026-06-24 22:14:50 +0800

    fix: remove fallback type matching configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,24 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router' // ◄ ADD THIS IMPORT
-import App from "./app/App";
+import { RouterProvider, createBrowserRouter } from 'react-router'
+import App from './app/App'
 import './index.css'
 import '../default_shadcn_theme.css'
 
+// Standard web route setup for single page apps in React Router v7
+const router = createBrowserRouter([
+  {
+    path: '*',
+    element: <App />,
+  }
+], {
+  future: {
+    v7_relativeSplatPath: true,
+  }
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> {/* ◄ WRAP APP IN BROWSERROUTER */}
-      <App />
-    </BrowserRouter>
-  </StrictMode>,)
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
