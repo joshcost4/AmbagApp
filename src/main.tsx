9902fdfa689a34e8b7819494f9e11cd2b1e5d143
unknown <costajoshua020@gmail.com>
2026-06-24 21:53:24 +0800

    fix: wrap App component in BrowserRouter context

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App";
-  import "./styles/index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router' // ◄ ADD THIS IMPORT
+import App from "./app/App";
+import './index.css'
+import '../default_shadcn_theme.css'
 
-  createRoot(document.getElementById("root")!).render(<App />);
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .catch((error) => console.warn('Service worker registration failed:', error));
-  });
-}  
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter> {/* ◄ WRAP APP IN BROWSERROUTER */}
+      <App />
+    </BrowserRouter>
+  </StrictMode>,)
